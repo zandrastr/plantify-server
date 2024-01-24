1ea65c6ccbfecb7b8a54bc4d2a18300c21b00ceb
Sandra S <zandrastr@hotmail.com>
2024-01-24 12:00:04 +0000

    create get plant route and update add plant route

--- routes/plant.routes.js
+++ routes/plant.routes.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/user.model');
 const Plant = require('../models/plant.model');
+const { default: mongoose } = require('mongoose');
 
-//Add a plant to database and/or user favorites
+// Get one plant from database
+router.get('/plants/:latinName', async (req, res, next) => {
+const { latinName } = req.params;
+
+try {
+    if (!latinName) {
+    res.status(400).json({ message: 'Could not find plant.' });
+    return;
+    }
+
+    let plant = await Plant.findOne({latinName})
+    res.status(200).json(plant);
+
+    } catch (error) {
+        res.status(400).json({message: 'Something went wrong while getting one plant', error });
+    }
+});
+
+//Add a plant to database and user favorites
 router.post('/plants/save', async(req, res, next) => {
 
     try {
@@ -14,6 +33,18 @@ router.post('/plants/save', async(req, res, next) => {
             return;
         }
 
+        if (!mongoose.Types.ObjectId.isValid(_id)) {
+            res.status(400).json({message: 'User id is not valid.'});
+            return;
+        }
+
+        const foundPlant = await Plant.findOne({latinName});
+
+        if (foundPlant) {
+            res.status(400).json({message: 'Plant already exists.'});
+            return;
+        }
+
         const foundUser = await User.findOne({_id});
 
         if (!foundUser) {
@@ -21,17 +52,10 @@ router.post('/plants/save', async(req, res, next) => {
             return;
         }
 
-        const foundPlant = await Plant.findOne({latinName});
-
-        if (foundPlant) {
-            await User.findByIdAndUpdate(_id, { $push: {favorites: foundPlant._id }}); 
-            res.status(200).json({message: 'Plant saved successfully', foundPlant});
-            return;
-        }
-
         const createdPlant = await Plant.create({name, latinName, description, sunNeeds, waterNeeds, imageUrl});
-        await User.findByIdAndUpdate(_id, { $push: {favorites: foundPlant._id }}); 
+        await User.findByIdAndUpdate(_id, { $push: {favorites: createdPlant._id }}); 
         res.status(200).json({message: 'Plant saved successfully', createdPlant});
+        
 
     } catch (error) {
         console.log("Something went wrong while saving plant:", error);
